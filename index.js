deca55d68f41ec014f76bcc79d4d422ea1f8fe40
Le Tuan <tuanlean1996@gmail.com>
2018-03-30 00:42:34 +0700

    add db

--- index.js
+++ index.js
@@ -8,5 +8,19 @@ app.get('/',
 (req, res) => {
     res.send("Hello super new World!")
         })
-        
+
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});        
+
 app.listen(PORT, () => console.log('Example app listening on port 5000!'))  
