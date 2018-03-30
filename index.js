02b5c68c2ae1bf705d7e8b04a1dd2b59968b3883
Le Tuan <tuanlean1996@gmail.com>
2018-03-30 09:37:59 +0700

    beatify4

--- index.js
+++ index.js
@@ -11,9 +11,51 @@ app.get('/',
 
 var pg = require('pg');
 
+//Get all from database
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      response.setHeader('Content-Type', 'application/json');
+      if (err)
+       { 
+        response.send(JSON.stringify({
+          status: 'error', 
+          data: err,
+          message: 'Request failed'
+          }));
+         console.error(err); response.send("Error " + err); 
+        }
+      else
+       {
+           
+    //     results.forEach(function(r) { 
+    //         r.id  + r.name
+    //  }); 
+    // response.status(200)
+    // .json({
+    //   status: 'success',
+    //   data: result.row[0],
+    //   message: 'Retrieved ALL puppies'
+    // });
+    
+    response.send(JSON.stringify({
+      status: 'success', 
+      data: result.rows,
+      message: 'Return test file'
+      }));
+          // response.send("Error " + result.rows[0].id  + " "+ result.rows[0].name); 
+        }//response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});        
+
+//Get one with param from database
+app.get('/db/:id', function (request, response) {
+  // const id = req.params.id
+  var nameId = parseInt(req.params.id);
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table where id='+ nameId, function(err, result) {
       done();
       response.setHeader('Content-Type', 'application/json');
       if (err)
