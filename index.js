25f9786f7b52f85b6501be84d909bb248c15984c
Le Tuan <tuanlean1996@gmail.com>
2018-03-31 12:20:07 +0700

    “updateapi”

--- index.js
+++ index.js
@@ -148,9 +148,9 @@ app.get('/convo/:id', function (request, response) {
 app.get('/temp', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function (err, client, done) {
     client.query(
-      "SELECT users.user_name,replies.rep_message FROM replies " +
-      " INNER JOIN users ON replies.rep_by = users.user_id " +
-      " WHERE related_to_convo = " + "1" + " ORDER BY rep_id ASC ",
+      "SELECT users.user_name,replies.rep_message,replies.related_to_convo FROM users " +
+      " INNER JOIN replies ON users.user_id = replies.rep_by " + 
+      " ORDER BY rep_id ASC ",
       function (err, result) {
         done();
         response.setHeader('Content-Type', 'application/json');
