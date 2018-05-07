991fd6f3ddf73fa8a83653bb131856198f216bb2
Le Tuan <tuanlean1996@gmail.com>
2018-05-07 22:39:50 +0700

    “updatedb”

--- index.js
+++ index.js
@@ -970,48 +970,48 @@ app.post('/authenticate/', function (request, response) {
   // } = request.query
 
   const credentials = auth(request);
-
 		if (!credentials) {
 			response.send(JSON.stringify({
         status: 'error',
         isSignin: false,
-        data: err,
         message: messFailed
       }));
 
 		} else {
-      response.send(JSON.stringify({
-        status: 'ok',
-        isSignin: true,
-        data: err,
-        message: messFailed
-      }));
-      // pg.connect(process.env.DATABASE_URL, function (err, client, done) {
-      //   response.setHeader('Content-Type', 'application/json');
-      //   // client.query('insert into test_table values ('+id+', \''+name+'\')', function(err, result) {
-      //   client.query(
-      //     "SELECT user_id,ref_cat_id from users where user_name ='" + name + "' and user_pw='" + pass + "'",
-      //     function (err, result) {
-      //       done();
-      //       if (err || result.rows.length < 1) {
-      //         response.send(JSON.stringify({
-      //           status: 'error',
-      //           isSignin: false,
-      //           data: err,
-      //           message: messFailed
-      //         }));
-      //         // console.error(err);
-      //         // response.send("Error " + err);
-      //       } else {
-      //         response.send(JSON.stringify({
-      //           status: 'success',
-      //           data: result.rows,
-      //           isSignin: true,
-      //           message: 'Here is your id User'
-      //         }));
-      //       }
-      //     });
-      // });
+      const name = credentials.name;
+      const pass = credentials.pass;
+      // console.log(credentials.name)      
+      // response.send(JSON.stringify({
+      //   status: 'ok',
+      //   isSignin: true,
+      //   message: messFailed
+      // }));
+      pg.connect(process.env.DATABASE_URL, function (err, client, done) {
+        response.setHeader('Content-Type', 'application/json');
+        // client.query('insert into test_table values ('+id+', \''+name+'\')', function(err, result) {
+        client.query(
+          "SELECT user_id,ref_cat_id from users where user_name ='" + name + "' and user_pw='" + pass + "'",
+          function (err, result) {
+            done();
+            if (err || result.rows.length < 1) {
+              response.send(JSON.stringify({
+                status: 'error',
+                isSignin: false,
+                data: err,
+                message: messFailed
+              }));
+              // console.error(err);
+              // response.send("Error " + err);
+            } else {
+              response.send(JSON.stringify({
+                status: 'success',
+                data: result.rows,
+                isSignin: true,
+                message: 'Here is your id User'
+              }));
+            }
+          });
+      });
     }
   
 });
